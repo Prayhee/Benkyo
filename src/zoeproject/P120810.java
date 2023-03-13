c2a5142525f737f2a47a5c9a7aee0887f1393b03
DESKTOP-9DAJIVN\zoe <zoe.ub24@gmail.com>
2023-03-13 18:11:04 +0900

    프로그래머스 나머지 구하기

---
+++ src/zoeproject/P120810.java
@@ -0,0 +1,21 @@
+package zoeproject;
+
+import java.util.Scanner;
+
+public class P120810 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Solution {
+    public int solution(int num1, int num2) {
+        Scanner sc = new Scanner(System.in);
+        
+    	int answer = num1%num2;
+        
+        return answer;
+    }
+}
