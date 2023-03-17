b904b229350e131846bc0c529c77b4e1f27290e9
DESKTOP-9DAJIVN\zoe <zoe.ub24@gmail.com>
2023-03-17 16:28:00 +0900

    프로그래머스 몫 구하기

---
+++ src/zoeproject/P120805.java
@@ -0,0 +1,16 @@
+package zoeproject;
+
+public class P120805 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class Solution {
+	    public int solution(int num1, int num2) {
+	        int answer = num1/num2;
+	        return answer;
+	    }
+	}
+
+}
