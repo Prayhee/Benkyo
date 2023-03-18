2272673aab171c641f70e047e3b7202de4e7ff61
DESKTOP-9DAJIVN\zoe <zoe.ub24@gmail.com>
2023-03-18 15:56:30 +0900

    프로그래머스 숫자 비교하기

---
+++ src/zoeproject/P120807.java
@@ -0,0 +1,20 @@
+package zoeproject;
+
+public class P120807 {
+
+	public static void main(String[] args) {
+
+	}
+	class Solution {
+	    public int solution(int num1, int num2) {
+	        int answer = 0;
+	        if (num1 == num2){
+	            answer = 1;
+	        } else {
+	            answer = -1;
+	        }
+	        return answer;
+	    }
+	}
+
+}
