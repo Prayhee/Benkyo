7d9e053fcb6fc19d865c16d9898a246f042ab009
DESKTOP-9DAJIVN\zoe <zoe.ub24@gmail.com>
2023-03-20 22:09:43 +0900

    프로그래머 각도기

---
+++ src/zoeproject/P120829.java
@@ -0,0 +1,24 @@
+package zoeproject;
+
+public class P120829 {
+
+	public static void main(String[] args) {
+
+	}
+	class Solution {
+	    public int solution(int angle) {
+	        int answer = 0;
+	        if (angle <= 89){
+	            answer = 1;
+	        }else if(angle == 90){
+	            answer = 2;
+	        }else if(angle <= 179){
+	            answer = 3;
+	        }else if(angle == 180){
+	            answer = 4;
+	        }
+	        return answer;
+	    }
+	}
+
+}
