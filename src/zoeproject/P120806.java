a1ea5eb4c18b81fb362881cd7890448549d0097a
DESKTOP-9DAJIVN\zoe <zoe.ub24@gmail.com>
2023-03-21 21:54:32 +0900

    프로그래머스 두 수의 나눗셈
    정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return
    하도록 soltuion 함수를 완성해주세요.

---
+++ src/zoeproject/P120806.java
@@ -0,0 +1,16 @@
+package zoeproject;
+
+public class P120806 {
+
+	public static void main(String[] args) {
+
+	}
+	class Solution {
+	    public float solution(float num1, float num2) {
+	    	float answer = (num1 / num2) * 1000;
+	        int answer2 = (int)answer;
+	        return answer2;
+	    }
+	}
+
+}
