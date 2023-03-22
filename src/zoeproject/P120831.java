11f132cff04d34f354be19cc6914494fe0e05040
DESKTOP-9DAJIVN\zoe <zoe.ub24@gmail.com>
2023-03-22 21:30:31 +0900

    프로그래머스 짝수의 합
    정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.

---
+++ src/zoeproject/P120831.java
@@ -0,0 +1,21 @@
+package zoeproject;
+
+public class P120831 {
+
+	public static void main(String[] args) {
+	}
+	class Solution {
+		public int solution(int n) {
+			int answer = 0;
+			int sum = 0;
+			
+			for (int i = 0; i <= answer; i++) {
+				System.out.println(i);
+				if (i % 2 == 0) {
+					answer++;
+				}
+			}
+			return answer;
+		}
+	}
+}
