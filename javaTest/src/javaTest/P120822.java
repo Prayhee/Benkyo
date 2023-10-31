91220549da7a784020ff64093ec5bbd53d5510ca
DESKTOP-9DAJIVN\zoe <zoe.ub24@gmail.com>
2023-10-31 20:47:55 +0900

    짱 멋지게 해결

---
+++ javaTest/src/javaTest/P120822.java
@@ -0,0 +1,15 @@
+package javaTest;
+/*문자열 my_string이 매개변수로 주어집니다.
+my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+*/
+public class P120822 {
+	class Solution {
+	    public String solution(String my_string) {
+	        String answer = "";
+	        
+	        StringBuffer strre = new StringBuffer(my_string);
+	        answer = strre.reverse().toString();
+	        return answer;
+	    }
+	}
+}
